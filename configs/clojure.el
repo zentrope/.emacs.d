138fafd4b821ebe0887aecbcdf94b7047f53e527
Keith Irwin <keith@zentrope.com>
2012-09-14 23:31:08 -0700

    Clojure mode flashes buffer and top-level form on eval w/ nrepl.

--- configs/clojure.el
+++ configs/clojure.el
@@ -29,10 +29,41 @@
   (ANY 2)
   (context 2))
 
+(defun kfi-clojure-flash-region (start end)
+  (let ((overlay (make-overlay start end)))
+    (overlay-put overlay 'face 'secondary-selection)
+    (run-with-timer 0.2 nil 'delete-overlay overlay)))
+
+(defun kfi-clojure-flash-buffer ()
+  (interactive)
+  (save-excursion (kfi-clojure-flash-region (point-min) (point-max))))
+
+(defun kfi-clojure-flash-defn ()
+  (interactive)
+  (save-excursion
+    (end-of-defun)
+    (let ((e (point)))
+      (beginning-of-defun)
+      (kfi-clojure-flash-region (point) e))))
+
+(defun kfi-clojure-compile-buffer ()
+  (interactive)
+  (kfi-clojure-flash-buffer)
+  (nrepl-eval-buffer))
+
+(defun kfi-clojure-compile-defn ()
+  (interactive)
+  (kfi-clojure-flash-defn)
+  (nrepl-eval-expression-at-point))
+
 (defun kfi-clojure-hook ()
   (autopair-mode -1)
   (paredit-mode 1)
   (setq indent-tabs-mode nil)
-  (local-set-key (kbd "RET") 'newline-and-indent))
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  (local-set-key (kbd "C-c C-c") 'kfi-clojure-compile-defn)
+  (define-key nrepl-interaction-mode-map (kbd "C-c C-k")
+    'kfi-clojure-compile-buffer)
+  (local-set-key (kbd "C-c k") 'kfi-clojure-compile-buffer))
 
 (add-hook 'clojure-mode-hook #'kfi-clojure-hook)
