a446f1db5910ff5d9745936b650f4edcc6745fb2
Keith Irwin <keith@zentrope.com>
2013-01-19 12:17:13 -0800

    Set up proper indentation for javascript.

--- configs/javascript.el
+++ configs/javascript.el
@@ -1,2 +1,7 @@
-
 (setq js-indent-level 2)
+
+(defun kfi-js-mode-hook ()
+  (setq indent-tabs-mode nil)
+  (local-set-key (kbd "RET") 'newline-and-indent))
+
+(add-hook 'js-mode-hook #'kfi-js-mode-hook)
