a239d8e82f77b6a284339f38c3ab08e98ae26264
Keith Irwin <keith@zentrope.com>
2013-02-06 00:33:52 -0800

    More terminal mode fixes to help print unicode chars.

--- configs/terminal.el
+++ configs/terminal.el
@@ -16,6 +16,9 @@
  ;; Should figure out a way to query the current
  ;; background color and subtitute it here.
  '(term-default-bg-color "#111111")
-;; '(term-default-fg-color "#bbbbbb")
- '(term-default-fg-color "#bbbbbb")
- )
+ '(term-default-fg-color "#888888"))
+
+(add-hook 'term-exec-hook
+          (function
+           (lambda ()
+             (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
