378941139872171c362a58e7893bd6f183fd2edf
Keith Irwin <keith@zentrope.com>
2013-05-16 07:08:41 -0700

    Have diffs updated after magit session.

--- configs/diff-hl.el
+++ configs/diff-hl.el
@@ -1,3 +1,8 @@
 (require 'diff-hl)
 
-(global-diff-hl-mode)
+(global-diff-hl-mode 1)
+
+(defadvice magit-quit-session
+  (after update-diff-hl activate)
+  (dolist (buffer (buffer-list))
+    (with-current-buffer buffer (diff-hl-update))))
