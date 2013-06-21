ffedd73c97f4356d1a00ceddad7acd3c9418a82a
Keith Irwin <keith@zentrope.com>
2013-06-21 16:25:49 -0700

    Added fancy symbols for clojure mode.

--- configs/clojure.el
+++ configs/clojure.el
@@ -1,3 +1,24 @@
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "λ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\)("
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "ƒ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\){"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "∈")
+                               nil))))))
+
 (require 'clojure-mode)
 
 (defface kfi-paren-face
