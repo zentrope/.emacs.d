8e192fd51674bd66467e8589338ff9b1920921a3
Keith Irwin <keith@zentrope.com>
2014-03-09 15:36:30 -0700

    Update to latest org-mode repo.

--- init.el
+++ init.el
@@ -11,6 +11,9 @@
 ;; (add-to-list 'package-archives
 ;;              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
+
 (package-initialize)
 
 (when (not package-archive-contents)
