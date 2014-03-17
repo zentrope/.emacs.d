a10bc5e8ba6384b17cf86b477031b9d582a1330c
Keith Irwin <keith@zentrope.com>
2014-03-16 21:48:41 -0700

    Start with org file.

--- configs/setup.el
+++ configs/setup.el
@@ -5,7 +5,10 @@
 (add-to-list 'load-path "~/.emacs.d/")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms `((".*" "~/.emacs.d/backups")))
+
 
 (when (equal system-type 'darwin)
   (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
@@ -14,3 +17,4 @@
 ;; Move to a flyspell config file?
 
 (setq-default ispell-program-name "/usr/local/bin/aspell")
+(setq initial-buffer-choice "/Users/keith/Dripbox/scratch.org")
