7a53237331a7a3a4fd17db71795f1cc5120936bc
Keith Irwin <keith@zentrope.com>
2014-04-16 22:52:17 -0700

    Cleanup.

--- configs/terminal.el
+++ configs/terminal.el
@@ -25,28 +25,3 @@
    (define-key term-raw-map (kbd "C-y") 'term-paste)
    (define-key term-raw-map (kbd "C-v") 'term-paste)
    (define-key term-raw-map (kbd "s-v") 'term-paste)))
-
-
-;; bold="\[\e[01;37m\]"
-;; off="\[\e[00m\]"
-
-;; green="\[\e[0;32m\]"
-;; cyan="\[\e[0;36m\]"
-;; blueish="\[\e[1;38;5;75m\]"
-
-;; wdir="${blueish}\w${off}"
-
-;; function gitb {
-;;   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-;;   echo " ("${ref#refs/heads/}")"
-;; }
-
-;; b1="${lblue}(${lgray}"
-;; b2="${lblue})${off}"
-
-;; if [ "${INSIDE_EMACS}" != "" ]; then
-;;     export TERM="xterm-256color"
-;;     echo "INSIDE EMACS"
-;; fi
-
-;; PS1=" ${cyan}eλ${off} ${bold}\w${off}${green}\$(gitb)${off} > "
