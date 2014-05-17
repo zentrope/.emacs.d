113ba008a0bb5dcfa3c9f777793f681f80c8bff3
Keith Irwin <keith@zentrope.com>
2014-05-16 21:00:40 -0700

    Maybe fix for backup thing?

--- init.el
+++ init.el
@@ -5,8 +5,8 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
-(setq auto-save-file-name-transforms `((".*" "~/.emacs.d/backups")))
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups/")))
+(setq auto-save-file-name-transforms `((".*" "~/.emacs.d/backups/")))
 
 (when (equal system-type 'darwin)
   (push "/usr/texbin" exec-path)
