347d5d797ee7a1293b6569dd9c146afd9c468df2
Keith Irwin <keith@zentrope.com>
2014-05-20 11:35:57 -0700

    Fix markdown output.

--- kfi/kfi-markdown.el
+++ kfi/kfi-markdown.el
@@ -8,6 +8,9 @@
 (add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
 
+(setq markdown-xhtml-header-content
+      "<style>body { margin: 0 auto; width: 600px;} </style>")
+
 (defun kfi-markdown-mode-hook ()
   (auto-fill-mode 1))
 
