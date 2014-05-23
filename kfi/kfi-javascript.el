fad5f0f00ad5e801ceaf7d20a93e08c88f47b5be
Keith Irwin <keith@zentrope.com>
2014-05-23 10:56:03 -0700

    Javascript back to a sane 2 char spacing.

--- kfi/kfi-javascript.el
+++ kfi/kfi-javascript.el
@@ -2,7 +2,7 @@
 
 (defun kfi-js-mode-hook ()
   (setq indent-tabs-mode nil)
-  (setq js-indent-level 4)
+  (setq js-indent-level 2)
   (local-set-key (kbd "RET") 'newline-and-indent))
 
 (add-hook 'js-mode-hook #'kfi-js-mode-hook)
