70a956b6dc6d2d04fb6a83dca9aec3feae968076
Keith Irwin <keith@zentrope.com>
2014-06-19 21:56:52 -0700

    Font lock for cider repl. Alas, not for output.

--- kfi/kfi-cider.el
+++ kfi/kfi-cider.el
@@ -3,6 +3,7 @@
 ;; https://github.com/clojure-emacs/cider
 
 (setq cider-popup-stacktraces nil)
+(setq cider-repl-use-clojure-font-lock t)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
 (provide 'kfi-cider)
