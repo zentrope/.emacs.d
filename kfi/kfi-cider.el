8b6de550dfdb12cfaac026fefc513d59d74f24c8
Keith Irwin <keith@zentrope.com>
2014-10-24 12:07:38 -0700

    Eldoc mode in Cider.

--- kfi/kfi-cider.el
+++ kfi/kfi-cider.el
@@ -7,5 +7,6 @@
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 (add-hook 'cider-repl-mode-hook 'company-mode)
 (add-hook 'cider-mode-hook 'company-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
 (provide 'kfi-cider)
