ed108afc445c4114bed324705246703eaf8d16fd
Keith Irwin <keith@zentrope.com>
2014-12-26 13:26:51 -0800

    More fixups for ERC.
    I can't tell of scrolltobottom is working properly.

--- kfi/kfi-erc.el
+++ kfi/kfi-erc.el
@@ -8,13 +8,22 @@
 
 (add-hook 'erc-mode-hook 'kfi-erc-mode-hook)
 
+;; Not sure if this is necessary.
+(progn (add-to-list 'erc-modules 'truncate)
+       (add-to-list 'erc-modules 'scrolltobottom)
+       (erc-update-modules))
+
+(erc-scrolltobottom-mode 1)
+
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 (setq erc-fill-prefix "    ")
 (setq erc-prompt (lambda () (concat "\n" (buffer-name) " >")))
-(set-face-attribute 'erc-prompt-face nil :foreground "darkorange" :background "#111111")
-(set-face-attribute 'erc-timestamp-face nil :foreground "gray25" :background "#111111")
+(set-face-attribute 'erc-prompt-face nil
+                    :foreground "darkorange" :background "#111111")
+(set-face-attribute 'erc-timestamp-face nil
+                    :foreground "gray25" :background "#111111")
 (setq erc-fill-column 78)
-(setq erc-scroll-to-bottom -1)
+(setq erc-scroll-to-bottom -2)
 (setq erc-truncate-buffer-on-save t)
 (setq erc-max-buffer-size 30000)
 
