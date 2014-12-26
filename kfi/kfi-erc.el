eb5946709891810fc741fa40f997e44bfa3edd2d
Keith Irwin <keith@zentrope.com>
2014-12-26 12:58:56 -0800

    Fix for ERC.
    Gotta require it before you can set faces and so on.

--- kfi/kfi-erc.el
+++ kfi/kfi-erc.el
@@ -1,5 +1,7 @@
 ;; config/erc.el
 
+(require 'erc)
+
 (defun kfi-erc-mode-hook ()
   (make-local-variable 'global-hl-line-mode)
   (setq global-hl-line-mode nil))
@@ -8,7 +10,7 @@
 
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
 (setq erc-fill-prefix "    ")
-(setq erc-prompt (lambda () (concat "\n" (buffer-name) " > ")))
+(setq erc-prompt (lambda () (concat "\n" (buffer-name) " >")))
 (set-face-attribute 'erc-prompt-face nil :foreground "darkorange" :background "#111111")
 (set-face-attribute 'erc-timestamp-face nil :foreground "gray25" :background "#111111")
 (setq erc-fill-column 78)
