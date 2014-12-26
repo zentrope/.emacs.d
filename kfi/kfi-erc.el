f5a71997fbc976163a0818f0c458cd99f0e6b1c9
Keith Irwin <keith@zentrope.com>
2014-12-26 11:35:54 -0800

    Some ERC fixups.

--- kfi/kfi-erc.el
+++ kfi/kfi-erc.el
@@ -7,5 +7,9 @@
 (add-hook 'erc-mode-hook 'kfi-erc-mode-hook)
 
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
-
+(setq erc-fill-prefix "    ")
+(setq erc-prompt (lambda () (concat "\n" (buffer-name) " > ")))
+(set-face-attribute 'erc-prompt-face nil :foreground "darkorange" :background nil)
+(set-face-attribute 'erc-timestamp-face nil :foreground "gray25" :background nil)
+(setq erc-fill-column 78)
 (provide 'kfi-erc)
