559c06eef4e1cfd122ab786292670a2110af6226
Keith Irwin <keith@zentrope.com>
2015-02-16 20:41:05 -0800

    Lighten up dark background to avoid blending in with other dark backgrounds.

--- kfi/kfi-theme.el
+++ kfi/kfi-theme.el
@@ -57,8 +57,8 @@
   (defun kfi-setup-default-theme-overrides ()
     ;;
     ;; Frame
-    (set-face-attribute 'fringe nil :background "#111111")
-    (set-face-attribute 'default nil :background "#111111")
+    (set-face-attribute 'fringe nil :background "#212021")
+    (set-face-attribute 'default nil :background "#212021")
     ;;
     ;; Font-lock
     (set-face-attribute font-lock-keyword-face nil :foreground "peru")
