0653b9d7fccfa4bb83c4062753c1c9625efcedd9
Keith Irwin <keith@zentrope.com>
2015-04-13 14:50:30 -0700

    Don't start linum-mode automatically.

--- kfi/kfi-linum.el
+++ kfi/kfi-linum.el
@@ -1,13 +1,12 @@
 (require 'linum)
 
-
 (set-face-attribute 'linum nil :foreground "#4a525a")
 (setq linum-format "%4d \u2502")
 
-(dolist (hook '(clojure-mode-hook
-                js-mode-hook
-                ;; emacs-lisp-mode-hook
-                scala-mode-hook))
-  (add-hook hook (lambda () (linum-mode t))))
+;; (dolist (hook '(clojure-mode-hook
+;;                 js-mode-hook
+;;                 ;; emacs-lisp-mode-hook
+;;                 scala-mode-hook))
+;;   (add-hook hook (lambda () (linum-mode t))))
 
 (provide 'kfi-linum)
