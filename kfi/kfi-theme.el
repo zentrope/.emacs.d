ba7f2b0a6eb8f968f6fdb4a936fd7a3ae2c71b66
Keith Irwin <keith@zentrope.com>
2015-07-16 11:31:36 -0700

    Black-hole background again.

--- kfi/kfi-theme.el
+++ kfi/kfi-theme.el
@@ -91,8 +91,10 @@
   (defun kfi-setup-default-theme-overrides ()
     ;;
     ;; Frame
-    (set-face-attribute 'fringe nil :background "#212021")
-    (set-face-attribute 'default nil :background "#212021")
+    ;; (set-face-attribute 'fringe nil :background "#212021")
+    ;; (set-face-attribute 'default nil :background "#212021")
+    (set-face-attribute 'fringe nil :background "black")
+    (set-face-attribute 'default nil :background "black")
     ;;
     ;; Font-lock
     (set-face-attribute font-lock-keyword-face nil :foreground "peru")
