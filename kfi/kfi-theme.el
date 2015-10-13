299426c37609af05d09f51f5ca408cb545edc980
Keith Irwin <keith@zentrope.com>
2015-10-12 19:18:47 -0700

    Light/Dark theme switcher.

--- kfi/kfi-theme.el
+++ kfi/kfi-theme.el
@@ -43,108 +43,122 @@
 (set-default 'cursor-type 'hollow)
 (setq ring-bell-function 'ignore)
 
-(setq dark-mode t)
+;;(setq dark-mode t)
+(setq dark-mode nil)
 
 ;; Transparency
 ;; (set-frame-parameter (selected-frame) 'alpha '(95 95))
 ;; (add-to-list 'default-frame-alist '(alpha 95 95))
 
-(when (and window-system (not dark-mode))
-  (global-hl-line-mode 1)
-  (set-face-attribute 'fringe nil :background "white")
-  (set-face-attribute 'default nil :background "white" :foreground "black")
-  (set-face-attribute 'default nil :family "Menlo" :height 120 :weight 'normal)
-
-  (defun kfi-setup-modeline ()
-    (set-face-attribute 'region nil :foreground nil :background "lavender")
-    (set-face-attribute 'hl-line nil :foreground nil :background "aliceblue"
-                        :underline nil)
-
-    (set-face-attribute 'mode-line nil :foreground "royalblue" :background "#dddddd"
-                        :family "Menlo" :height 100 :weight 'normal
-                        :box '(:line-width 4 :color "#dddddd" :style nil))
-
-    (set-face-attribute 'mode-line-inactive nil :foreground "#666666"
-                        :background "#bbbbbb" :family "Menlo" :height 100
-                        :weight 'normal :italic t
-                        :box '(:line-width 4 :color "#bbbbbb" :style nil)))
-  (kfi-setup-modeline)
-  (add-to-list 'default-frame-alist '(cursor-color . "dodgerblue"))
-
-  (defun kfi-setup-light-theme-overrides ()
-    ;;
-    ;; Frame
+(defun kfi-set-theme-mode ()
+  (when (and window-system (not dark-mode))
+    (global-hl-line-mode 1)
     (set-face-attribute 'fringe nil :background "white")
-    (set-face-attribute 'default nil :background "white")
-    ;;
-    ;; Font-lock (mainly Clojure)
-    (set-face-attribute font-lock-keyword-face nil :foreground "#a61d5d" :weight 'normal)
-    ;; Java interop
-    (set-face-attribute font-lock-preprocessor-face nil :foreground "sienna")
-    (set-face-attribute font-lock-function-name-face nil :foreground "black")
-    (set-face-attribute font-lock-variable-name-face nil :foreground "dodgerblue")
-    (set-face-attribute font-lock-constant-face nil :foreground "#0086b3")
-    (set-face-attribute font-lock-string-face nil :slant 'normal :foreground "#13208e")
-    (set-face-attribute font-lock-comment-face nil :slant 'normal
-                        :foreground "mediumpurple" :height 120)
-    (set-face-attribute font-lock-type-face nil :foreground "#795da3"))
+    (set-face-attribute 'default nil :background "white" :foreground "black")
+    (set-face-attribute 'default nil :family "Menlo" :height 120 :weight 'normal)
 
-  (kfi-setup-light-theme-overrides)
-  ;;
-  )
+    (defun kfi-setup-modeline ()
+      (set-face-attribute 'region nil :foreground nil :background "lavender")
+      (set-face-attribute 'hl-line nil :foreground nil :background "aliceblue"
+                          :underline nil)
 
+      (set-face-attribute 'mode-line nil :foreground "royalblue" :background "#dddddd"
+                          :family "Menlo" :height 100 :weight 'normal
+                          :box '(:line-width 4 :color "#dddddd" :style nil))
 
-(when (and window-system dark-mode)
-  (global-hl-line-mode 1)
+      (set-face-attribute 'mode-line-inactive nil :foreground "#666666"
+                          :background "#bbbbbb" :family "Menlo" :height 100
+                          :weight 'normal :italic t
+                          :box '(:line-width 4 :color "#bbbbbb" :style nil)))
+    (kfi-setup-modeline)
+    (add-to-list 'default-frame-alist '(cursor-color . "dodgerblue"))
 
-  (invert-face 'default)
+    (defun kfi-setup-light-theme-overrides ()
+      ;;
+      ;; Frame
+      (set-face-attribute 'fringe nil :background "white")
+      (set-face-attribute 'default nil :background "white")
+      ;;
+      ;; Font-lock (mainly Clojure)
+      (set-face-attribute font-lock-keyword-face nil :foreground "#a61d5d" :weight 'normal)
+      ;; Java interop
+      (set-face-attribute font-lock-preprocessor-face nil :foreground "sienna")
+      (set-face-attribute font-lock-function-name-face nil :foreground "black")
+      (set-face-attribute font-lock-variable-name-face nil :foreground "dodgerblue")
+      (set-face-attribute font-lock-constant-face nil :foreground "#0086b3")
+      (set-face-attribute font-lock-string-face nil :slant 'normal :foreground "gray60")
+      (set-face-attribute font-lock-comment-face nil :slant 'normal
+                          :foreground "mediumpurple" :height 120)
+      (set-face-attribute font-lock-type-face nil :foreground "#795da3"))
 
-  (defun kfi-setup-default-theme-overrides ()
-    ;;
-    ;; Frame
-    ;; (set-face-attribute 'fringe nil :background "#212021")
-    ;; (set-face-attribute 'default nil :background "#212021")
-    (set-face-attribute 'fringe nil :background "#151525")
-    (set-face-attribute 'default nil :background "black")
-    ;;
-    ;; Font-lock
-    (set-face-attribute font-lock-keyword-face nil :foreground "peru")
-    (set-face-attribute font-lock-preprocessor-face nil :foreground "steelblue")
-    (set-face-attribute font-lock-function-name-face nil :foreground "cornflowerblue")
-    (set-face-attribute font-lock-constant-face nil :slant 'italic :foreground "mediumslateblue")
-    (set-face-attribute font-lock-string-face nil :slant 'normal :foreground "plum")
-    (set-face-attribute font-lock-variable-name-face nil :foreground "sandybrown")
-    (set-face-attribute font-lock-comment-face nil :slant 'normal :foreground "mediumpurple"))
-
-  (kfi-setup-default-theme-overrides)
-
-  ;; (set-face-attribute 'hl-line nil :foreground nil :underline nil)
-  ;; (set-face-foreground 'highlight nil)
-
-  (set-face-attribute 'hl-line nil :foreground nil :background "gray15"
-                      :underline nil)
-
-  (defun kfi-setup-modeline ()
-    (set-face-attribute 'region nil :foreground nil :background "blue")
-
-    (set-face-attribute 'mode-line nil
-                        :foreground "white"
-                        :background "dodgerblue4"
-                        :family "Monaco" :height 100 :weight 'normal
-                        :box '(:line-width 2 :color "dodgerblue4" :style nil))
-
-    (set-face-attribute 'mode-line-inactive nil :foreground "gray60"
-                        :background "gray15" :family "Menlo" :height 100
-                        :weight 'normal :italic nil
-                        :box '(:line-width 2 :color "gray17" :style nil))
+    (kfi-setup-light-theme-overrides)
     ;;
     )
 
-  (kfi-setup-modeline)
 
-  (add-to-list 'default-frame-alist '(cursor-color . "dodgerblue")))
+  (when (and window-system dark-mode)
+    (global-hl-line-mode 1)
+
+    (invert-face 'default)
+
+    (defun kfi-setup-default-theme-overrides ()
+      ;;
+      ;; Frame
+      ;; (set-face-attribute 'fringe nil :background "#212021")
+      ;; (set-face-attribute 'default nil :background "#212021")
+      (set-face-attribute 'fringe nil :background "#151525")
+      (set-face-attribute 'default nil :background "black")
+      ;;
+      ;; Font-lock
+      (set-face-attribute font-lock-keyword-face nil :foreground "peru")
+      (set-face-attribute font-lock-preprocessor-face nil :foreground "steelblue")
+      (set-face-attribute font-lock-function-name-face nil :foreground "cornflowerblue")
+      (set-face-attribute font-lock-constant-face nil :slant 'italic :foreground "mediumslateblue")
+      (set-face-attribute font-lock-string-face nil :slant 'normal :foreground "gray60")
+      (set-face-attribute font-lock-variable-name-face nil :foreground "sandybrown")
+      (set-face-attribute font-lock-comment-face nil :slant 'normal :foreground "mediumpurple"))
+
+    (kfi-setup-default-theme-overrides)
+
+    ;; (set-face-attribute 'hl-line nil :foreground nil :underline nil)
+    ;; (set-face-foreground 'highlight nil)
+
+    (set-face-attribute 'hl-line nil :foreground nil :background "gray15"
+                        :underline nil)
+
+    (defun kfi-setup-modeline ()
+      (set-face-attribute 'region nil :foreground nil :background "blue")
+
+      (set-face-attribute 'mode-line nil
+                          :foreground "white"
+                          :background "dodgerblue4"
+                          :family "Monaco" :height 100 :weight 'normal
+                          :box '(:line-width 2 :color "dodgerblue4" :style nil))
+
+      (set-face-attribute 'mode-line-inactive nil :foreground "gray60"
+                          :background "gray15" :family "Menlo" :height 100
+                          :weight 'normal :italic nil
+                          :box '(:line-width 2 :color "gray17" :style nil))
+      ;;
+      )
+
+    (kfi-setup-modeline)
+
+    (add-to-list 'default-frame-alist '(cursor-color . "dodgerblue"))))
 
 (when window-system
+  (kfi-set-theme-mode)
+
+  (defun kfi-go-dark ()
+    (interactive)
+    (setq dark-mode t)
+    (kfi-set-theme-mode))
+
+  (defun kfi-go-light ()
+    (interactive)
+    (setq dark-mode nil)
+    (kfi-set-theme-mode))
+
   ;;
   ;; Not sure if this works.
   (defun kfi-fix-echo-area ()
