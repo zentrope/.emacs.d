0f28c758eef94b9954d77b1eac7c2dbee61fc048
Keith Irwin <keith@zentrope.com>
2017-07-06 16:04:16 -0700

    Fix comp warnings on fuzzy match stuff.

--- kfi/kfi-fuzzy.el
+++ kfi/kfi-fuzzy.el
@@ -1,25 +1,37 @@
+;;
+;;
+(defvar ido-cur-list nil
+  "Fix for byte compile warning with ido-completing-read+. Far as
+   I can tell, ido-cur-list isn't defined anywhere (anymore?).")
+
 (use-package ido
   :ensure t
-  :init (progn (ido-mode 1)
-               (ido-everywhere 1)
-               (setq ido-enable-flex-matching t)
+  :config
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (setq ido-enable-flex-matching t)
 
-               (use-package ido-completing-read+
-                 :ensure t
-                 :init (ido-ubiquitous-mode 1))
+  (use-package ido-completing-read+
+    :ensure t
+    :config
+    (ido-ubiquitous-mode 1))
 
-               (use-package flx-ido ;; fuzzy matching
-                 :ensure t
-                 :init (progn (flx-ido-mode 1)
-                              (setq ido-enable-flex-matching t)
-                              (setq ido-use-faces nil)))
-               (use-package ido-vertical-mode
-                 :ensure t
-                 :init (progn (ido-vertical-mode 1)
-                              (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)))))
+  (use-package flx-ido
+    ;; Makes fuzzy match prettier?
+    :ensure t
+    :config
+    (flx-ido-mode 1)
+    (setq ido-use-faces nil))
+
+  (use-package ido-vertical-mode
+    :ensure t
+    :config
+    (ido-vertical-mode 1)
+    (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)))
 
 (use-package smex
   :ensure t
-  :bind ("M-x" . smex))
+  :bind
+  ("M-x" . smex))
 
 (provide 'kfi-fuzzy)
