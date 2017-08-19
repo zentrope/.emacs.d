4d4997802a9d48489ec53f1b225520de158924c8
Keith Irwin <keith@zentrope.com>
2017-08-18 21:29:15 -0700

    Base light/dark themes on defaults, not atom-dark.

--- kfi/kfi-theme.el
+++ kfi/kfi-theme.el
@@ -1,84 +1,74 @@
-(use-package atom-one-dark-theme
-  :if window-system
-  :ensure t
-  :config
+;;
+;;
+;;
+(defun kfi/dark ()
+  (interactive)
+  (load-theme 'atom-one-dark t)
+  (set-face-attribute 'fringe nil :background kfi/background)
+  (set-face-attribute 'default nil :background kfi/background)
 
-  (defun kfi-dark ()
-    (interactive)
-    (load-theme 'atom-one-dark t)
-    (set-face-attribute 'fringe nil :background kfi/background)
-    (set-face-attribute 'default nil :background kfi/background)
+  (set-face-attribute 'mode-line nil :background "#292c34")
 
-    (set-face-attribute 'mode-line nil :background "#292c34")
+  (set-face-attribute 'mode-line nil
+                      :foreground "#ccc"
+                      :background "#374350"
+                      :underline nil
+                      :family "Monaco" :height 100 :weight 'normal
+                      :box '(:line-width 2 :color "#374350" :style nil))
 
-    (set-face-attribute 'mode-line nil
-                        :foreground "#ccc"
-                        :background "#374350"
-                        :underline nil
-                        :family "Monaco" :height 100 :weight 'normal
-                        :box '(:line-width 2 :color "#374350" :style nil))
+  (set-face-attribute 'mode-line-inactive nil :foreground "gray60"
+                      :background "gray15" :family "Monaco" :height 100
+                      :weight 'normal :italic nil
+                      :box '(:line-width 2 :color "gray15" :style nil))
 
-    (set-face-attribute 'mode-line-inactive nil :foreground "gray60"
-                        :background "gray15" :family "Monaco" :height 100
-                        :weight 'normal :italic nil
-                        :box '(:line-width 2 :color "gray15" :style nil))
+  (set-face-attribute 'font-lock-comment-face nil :foreground "firebrick")
 
-    (set-face-attribute 'font-lock-comment-face nil :foreground "firebrick")
+  (set-face-attribute 'region nil :background "#3E4451")
 
-    (set-face-attribute 'region nil :background "#3E4451")
+  ;; Linum customization
+  (set-face-attribute 'linum nil :foreground "#555" :background "#111")
 
-    ;; Linum customization
-    (set-face-attribute 'linum nil :foreground "#555")
-    (set-face-attribute 'linum nil :background "#111")
+  ;; ERC customizations
+  (set-face-attribute 'erc-prompt-face nil
+                      :foreground "darkorange"
+                      ;;:background "#212121"
+                      :background kfi/background
+                      )
 
-    ;; ERC customizations
-    (set-face-attribute 'erc-prompt-face nil
-                        :foreground "darkorange"
-                        ;;:background "#212121"
-                        :background kfi/background
-                        )
+  (set-face-attribute 'erc-timestamp-face nil
+                      :foreground "gray30"
+                      ;; :background "#292c34"
+                      :background kfi/background))
 
-    (set-face-attribute 'erc-timestamp-face nil
-                        :foreground "gray30"
-                        ;; :background "#292c34"
-                        :background kfi/background))
+(defun kfi/light ()
+  (interactive)
+  (disable-theme 'atom-one-dark)
+  (set-face-attribute 'fringe nil :background "#fff")
+  (set-face-attribute 'default nil :foreground "#444" :background "white")
+  (set-face-background hl-line-face "gray90")
 
-  (defun kfi-light ()
-    (interactive)
-    (disable-theme 'atom-one-dark)
-    (set-face-attribute 'fringe nil :background "#fff")
-    (set-face-attribute 'default nil :foreground "#444" :background "white")
-    (set-face-background hl-line-face "gray90")
+  (set-face-attribute 'mode-line nil
+                      :foreground "dodgerblue"
+                      :background "gray90"
+                      :underline nil
+                      :family "Monaco" :height 100 :weight 'normal
+                      :box '(:line-width 2 :color "gray90" :style nil))
 
-    (set-face-attribute 'mode-line nil
-                        :foreground "dodgerblue"
-                        :background "gray90"
-                        :underline nil
-                        :family "Monaco" :height 100 :weight 'normal
-                        :box '(:line-width 2 :color "gray90" :style nil))
+  (set-face-attribute 'mode-line-inactive nil :foreground "gray60"
+                      :background "#f2f2f2" :family "Monaco" :height 100
+                      :weight 'normal :italic nil
+                      :box '(:line-width 2 :color "#f2f2f2" :style nil))
 
-    (set-face-attribute 'mode-line-inactive nil :foreground "gray60"
-                        :background "#f2f2f2" :family "Monaco" :height 100
-                        :weight 'normal :italic nil
-                        :box '(:line-width 2 :color "#f2f2f2" :style nil))
+  (set-face-attribute 'region nil :background "aquamarine")
 
-    ;;(sml/apply-theme 'light)
-    (set-face-attribute 'region nil :background "aquamarine")
+  ;; Linum customization
+  (set-face-attribute 'linum nil :foreground "#888" :background "#f2f2f2")
 
-    ;; Linum customization
-    (set-face-attribute 'linum nil :foreground "#888" :background "#f2f2f2")
+  ;; ERC customizations
+  (set-face-attribute 'erc-prompt-face nil
+                      :foreground "dodgerblue" :background "white")
+  (set-face-attribute 'erc-timestamp-face nil
+                      :foreground "dodgerblue" :background "white"))
 
-    ;; ERC customizations
-    (set-face-attribute 'erc-prompt-face nil
-                        :foreground "dodgerblue" :background "white")
-    (set-face-attribute 'erc-timestamp-face nil
-                        :foreground "dodgerblue" :background "white"))
-
-  (when window-system
-    ;;(kfi-dark)
-    (kfi-light))
-
- ;; end use-package
- )
-
+(kfi/light)
 (provide 'kfi-theme)
