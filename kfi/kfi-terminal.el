a084ee8739a2bd9be20c7fa472cb0483bccb010f
Keith Irwin <keith@zentrope.com>
2017-08-24 12:00:39 -0700

    Make terminal mode unset line numbers.

--- kfi/kfi-terminal.el
+++ kfi/kfi-terminal.el
@@ -18,29 +18,28 @@
   (ad-activate 'term-char-mode)
 
   (setq multi-term-program "/bin/bash")
-  (set-face-attribute 'term nil :inherit 'default ;;:background kfi/background
-                      )
-  (set-face-attribute 'term nil :inherit 'default ;; :foreground "#aaaaaa"
-                      )
+  (set-face-attribute 'term nil :inherit 'default)
+  (set-face-attribute 'term nil :inherit 'default)
   (set-face-attribute 'term-color-cyan nil :foreground "dodgerblue")
   (set-face-attribute 'term-color-black nil :foreground "gray50")
   (set-face-attribute 'term-color-yellow nil :foreground "peru")
-
-  ;; (when (string= (face-attribute 'default :background) "White")
-  ;;   (set-face-attribute 'term nil :foreground "#444444" :background "white")
-  ;;   (set-face-attribute 'term-color-white nil :foreground "black"))
 
   (add-hook 'term-exec-hook
             (function
              (lambda ()
                (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
 
-  ;; Allow pasting text to be sent to the underlying shell process.
-  (add-hook
-   'term-mode-hook
-   (lambda ()
-     (define-key term-raw-map (kbd "C-y") 'term-paste)
-     (define-key term-raw-map (kbd "C-v") 'term-paste)
-     (define-key term-raw-map (kbd "s-v") 'term-paste))))
+  (defun kfi/inhibit-line-numbers ()
+    (add-hook 'after-change-major-mode-hook
+              (lambda ()
+                (display-line-numbers-mode -1))))
+
+  (defun kfi/term-allow-pasting-to-shell ()
+    (define-key term-raw-map (kbd "C-y") 'term-paste)
+    (define-key term-raw-map (kbd "C-v") 'term-paste)
+    (define-key term-raw-map (kbd "s-v") 'term-paste))
+
+  (add-hook 'term-mode-hook 'kfi/inhibit-line-numbers)
+  (add-hook 'term-mode-hook 'kfi/term-allow-pasting-to-shell))
 
 (provide 'kfi-terminal)
