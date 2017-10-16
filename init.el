8f0107c96c5abe765e710cb1cceb79fc0ac9cbbe
Keith Irwin <keith@zentrope.com>
2017-10-16 00:05:06 -0700

    Reset to stable cider extension for now.

--- init.el
+++ init.el
@@ -34,10 +34,10 @@
 ;; Development
 (setq package-archives
       (list '("melpa"        . "https://melpa.org/packages/")
-            ;;'("melpa-stable" . "https://stable.melpa.org/packages/")
+            '("melpa-stable" . "https://stable.melpa.org/packages/")
             '("gnu"          . "http://elpa.gnu.org/packages/")))
 
-;;(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 
 (package-initialize)
 
