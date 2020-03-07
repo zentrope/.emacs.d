f291255cf20c0aa2cfe56d3203654a5b754f167c
Keith Irwin <keith@zentrope.com>
2020-03-07 10:59:00 -0800

    Add Java LSP.

--- kfi/kfi-basics.el
+++ kfi/kfi-basics.el
@@ -20,6 +20,38 @@
 
 ;;(setq use-package-verbose t)
 
+;; --- java ---
+
+(use-package yasnippet
+  :ensure t)
+
+(use-package lsp-mode
+  :ensure t)
+
+(use-package hydra
+  :ensure t)
+
+(use-package company-lsp
+  :ensure t)
+
+(use-package lsp-ui
+  :ensure t)
+
+(use-package lsp-java
+  :ensure t
+  :after lsp
+  :config (add-hook 'java-mode-hook #'lsp))
+
+(use-package dap-mode
+  :ensure t
+  :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+
+;; --- /java ---
+
+
 ;; --- theme ---
 
 (defvar doom-themes-treemacs-theme)
