6dc322c21ca1bad33d84e93b24e66ca0a30a3ce4
Keith Irwin <keith@zentrope.com>
2020-03-08 17:43:29 -0700

    Reviews ibuffer, add javasrc org snippet.

--- kfi/kfi-basics.el
+++ kfi/kfi-basics.el
@@ -225,28 +225,6 @@
 (use-package htmlize
   :ensure t)
 
-(use-package ibuffer
-  :bind (("C-x C-b" . ibuffer)
-         ("<C-tab>" . ibuffer))
-  :config
-  (defvar ibuffer-saved-filter-groups
-    (quote (("default"
-             ("org" (mode . org-mode))
-             ("clojure" (mode . clojure-mode))
-             ("clojure-script" (mode . clojurescript-mode))
-             ("python" (mode . python-mode))
-             ("haki-script" (mode . lisp-mode))
-             ("go-lang" (mode . go-mode))
-             ("web" (or (mode . css-mode)
-                        (mode . html-mode)
-                        (mode . js-mode)))
-             ("erc" (name . "^\\#"))
-             ("elisp" (mode . emacs-lisp-mode))
-             ("dirs" (mode . dired-mode))
-             ("temps" (name . "^\\*.*\\*$"))))))
-  (add-hook 'ibuffer-mode-hook
-            '(lambda ()
-               (ibuffer-switch-to-saved-filter-groups "default"))))
 
 (declare-function ido-everywhere "ido.el")
 
