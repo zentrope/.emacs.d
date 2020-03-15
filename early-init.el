5693149a3185ad0a37ca8d8b8821e5dbad04e5bd
Keith Irwin <keith@zentrope.com>
2020-03-15 15:50:33 -0700

    Add GC performance tweak, quote snippet.

--- early-init.el
+++ early-init.el
@@ -1,2 +1,6 @@
 ;; [[https://protesilaos.com/dotemacs/#h:584c3604-55a1-49d0-9c31-abe46cb1f028]]
 (setq frame-inhibit-implied-resize t)
+(setq gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold 100000000)
+(add-hook 'after-init-hook (lambda ()
+                             (setq gc-cons-threshold gc-cons-threshold-original)))
