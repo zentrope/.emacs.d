f059e9c20636f6bfa9f58fbaa9f8a189afab3d29
Keith Irwin <keith@zentrope.com>
2020-03-18 19:56:05 -0700

    Move basic window config to early-init.

--- init.el
+++ init.el
@@ -18,12 +18,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (display-graphic-p)
-  (menu-bar-mode 1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (tooltip-mode -1))
-
 (require 'package)
 
 ;; Development
